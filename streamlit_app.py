3764fe450d33f4471776e1fb2a126eb2c96ab70a
tony hollaar <thollaar@gmail.com>
2023-04-30 18:39:15 -0400

    add results_df to session_state

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,7 +79,9 @@ st.write("")
 # define tabs of data pipeline for user to browse through
 tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9, tab10 = st.tabs(["Load🚀", "Explore🕵️‍♂️", "Clean🧹", "Engineer🧰", "Prepare🧪", "Select🍏", "Train🔢", "Evaluate🎯", "Tune⚙️", "Forecast🔮"])
 # Create a global pandas DataFrame to hold model_name and mape values
-results_df = pd.DataFrame(columns=['model_name', 'mape', 'rmse', 'r2', 'features', 'model settings'])
+#results_df = pd.DataFrame(columns=['model_name', 'mape', 'rmse', 'r2', 'features', 'model settings'])
+if 'results_df' not in st.session_state:
+    st.session_state['results_df'] = pd.DataFrame(columns=['model_name', 'mape', 'rmse', 'r2', 'features', 'model settings'])
 ###############################################################################
 # FUNCTIONS
 ###############################################################################
